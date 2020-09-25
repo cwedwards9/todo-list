b21740528d4038ac61bce9bb7e1ad7689dcb45fb
Chase Edwards <chase.w.edwards9@gmail.com>
2020-09-25 12:26:48 -0400

    Refactor code, add function that updates the list and is called after an addition or deletion of the list

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // start off with an empty array (for now, add local storage later)
 var items = [];
 
-// When a user presses enter, the text in the input is added to the list
+
+// When a user presses enter, the text in the input is added to the array and to the list
 $("#todoInput").on("keypress", function(e){
     if(e.which === 13){
         // get value and push to array
@@ -12,11 +13,8 @@ $("#todoInput").on("keypress", function(e){
             items.push(item);
         }
         
-        // Empty list before we loop through the array and add its items to the list
-        $("ul").empty();
-        for(var i = 0; i < items.length; i++){
-            $("ul").append("<li data=" + i + "><span><i class='fa fa-trash'></i></span>" + items[i] + "</li>");
-        }
+        updateListDisplay(items);
+
         // Clear the input 
         $(this).val("");
     }
@@ -25,20 +23,30 @@ $("#todoInput").on("keypress", function(e){
 
 // When a user clicks on the trash can icon (inside span), delete the entire parent li
 $("ul").on("click", "span", function(){
-    $(this).parent().remove();
-    var index = $(this).parent().attr("data");
+    var deleteItem = $(this).parent();
+    
     // Delete the corresponding element in the array
+    var index = deleteItem.attr("data");
     items.splice(index, 1);
 
-    // Refresh the list to reflect changes
-    $("ul").empty();
-    for(var i = 0; i < items.length; i++){
-        $("ul").append("<li data=" + i + "><span><i class='fa fa-trash'></i></span>" + items[i] + "</li>");
-    }
+    // Delete the li from the html
+    deleteItem.remove();
+
+    updateListDisplay(items);
 });
 
 
 // When a user clicks on the list item itself, the item is crossed off, but stays on the list
 $("ul").on("click", "li", function(){
     $(this).toggleClass("complete");
-})
+});
+
+
+// update the list display
+function updateListDisplay(arr){
+    // Empty list before we loop through the array and add its updated items to the list
+    $("ul").empty();
+    for(var i = 0; i < arr.length; i++){
+        $("ul").append("<li data=" + i + "><span><i class='fa fa-trash'></i></span>" + arr[i] + "</li>");
+    }
+}
