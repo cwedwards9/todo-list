d3a8114390f497b3eba0492e037b389889897b5e
Chase Edwards <chase.w.edwards9@gmail.com>
2020-10-14 16:11:35 -0400

    Change the items array to an array of objects which includes a new property for keeping track of list item status for each array item, (complete or in-progress) and then on click, update the html (text-decoration) to reflect the list item status from the item in the array.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const items = JSON.parse(localStorage.getItem("items")) || [];
 updateListDisplay(items);
 
 
-// When a user presses enter, the text in the input is added to the array and to the list
+// When a user presses enter, the text in the input is added to the array of objects and to the list
 $("#todoInput").on("keypress", function(e){
     if(e.which === 13){
         // get value and push to array
@@ -12,7 +12,12 @@ $("#todoInput").on("keypress", function(e){
         if(item === ""){
             return;
         } else {
-            items.push(item);
+            items.push(
+                {
+                    desc: item,
+                    status: "in-progress"
+                }
+            );
         }
         
         updateListDisplay(items);
@@ -24,7 +29,9 @@ $("#todoInput").on("keypress", function(e){
 
 
 // When a user clicks on the trash can icon (inside span), delete the entire parent li
-$("ul").on("click", "span", function(){
+$("ul").on("click", "span", function(e){
+    e.stopPropagation();
+
     let deleteItem = $(this).parent();
     
     // Delete the corresponding element in the array
@@ -38,18 +45,31 @@ $("ul").on("click", "span", function(){
 });
 
 
-// When a user clicks on the list item itself, the item is crossed off, but stays on the list
+// When a user clicks on the list item itself, the item's status toggles to 'complete' or 'in-progress', but stays on the list
 $("ul").on("click", "li", function(){
-    $(this).toggleClass("complete");
+    var clickedItem = $(this);
+    var index = clickedItem.attr("data");
+
+    if(clickedItem.attr("status") === "in-progress"){
+        items[index].status = "complete";
+    } else if (clickedItem.attr("status") === "complete"){
+        items[index].status = "in-progress";
+    }
+    
+    updateListDisplay(items);
+    
 });
 
 
-// update the list display
+// update the list display with data and status attribute and class status as well as the todo item
 function updateListDisplay(arr){
     // Empty list before we loop through the array and add its updated items to the list
     $("ul").empty();
     for(let i = 0; i < arr.length; i++){
-        $("ul").append("<li data=" + i + "><span><i class='fa fa-trash'></i></span>" + arr[i] + "</li>");
+        let item = "<li data=" + i + " " + "status=" + arr[i].status + " " + "class=" + arr[i].status + "><span><i class='fa fa-trash'></i></span>" + arr[i].desc + "</li>";
+
+        $("ul").append(item);
+        
     }
 
     // Save item to array in local storage
